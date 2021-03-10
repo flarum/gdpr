402efee04c406f91216bdd8fdd77849ef7c6e484
Ian Morland <ian@morland.me>
2021-03-10 22:03:57 +0000

    Update translation key

--- resources/views/confirm-erasure.blade.php
+++ resources/views/confirm-erasure.blade.php
@@ -1,4 +1,4 @@
-{!! $translator->trans('blomstra-gdpr.email.export_available.body', [
+{!! $translator->trans('blomstra-gdpr.email.confirm_erasure.body', [
     "{display_name}" => $user->display_name,
     "{erasure_confirm_url}" => $url->to('forum')->route('gdpr.erasure-confirm'),
 ]) !!}
