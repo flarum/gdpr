9dcde952dbc67c9dece6998d84cf90d24a1c857e
Ian Morland <ian@morland.me>
2023-11-01 21:29:20 +0000

    chore: format

--- js/src/forum/addAnonymousBadges.tsx
+++ js/src/forum/addAnonymousBadges.tsx
@@ -6,12 +6,12 @@ import ItemList from 'flarum/common/utils/ItemList';
 import type Mithril from 'mithril';
 
 export default function () {
-    extend(User.prototype, 'badges', function (badges: ItemList<Mithril.Children>) {
-        if (this.anonymized()) {
-            badges.add(
-                'anonymized',
-                <Badge label={app.translator.trans('blomstra-gdpr.forum.badges.anonymized_user')} icon="fas fa-user-secret" type="anonymized" />
-            );
-        }
-    });
+  extend(User.prototype, 'badges', function (badges: ItemList<Mithril.Children>) {
+    if (this.anonymized()) {
+      badges.add(
+        'anonymized',
+        <Badge label={app.translator.trans('blomstra-gdpr.forum.badges.anonymized_user')} icon="fas fa-user-secret" type="anonymized" />
+      );
+    }
+  });
 }
