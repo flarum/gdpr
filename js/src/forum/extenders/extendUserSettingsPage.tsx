21515705cdf6165093f8bc5d14bf95a95e54076f
Sami Mazouz <sychocouldy@gmail.com>
2024-10-25 16:35:14 +0100

    chore(2.0): adapt to extending lazy modules
    Some Flarum modules are now lazy loaded. Extending them requires a different approach.

--- js/src/forum/extenders/extendUserSettingsPage.tsx
+++ js/src/forum/extenders/extendUserSettingsPage.tsx
@@ -1,7 +1,6 @@
 import app from 'flarum/forum/app';
-import { extend } from 'flarum/common/extend';
+import { extend, override } from 'flarum/common/extend';
 import ItemList from 'flarum/common/utils/ItemList';
-import SettingsPage from 'flarum/forum/components/SettingsPage';
 import FieldSet from 'flarum/common/components/FieldSet';
 import type Mithril from 'mithril';
 import Button from 'flarum/common/components/Button';
@@ -9,7 +8,7 @@ import RequestErasureModal from '../components/RequestErasureModal';
 import RequestDataExportModal from '../../common/components/RequestDataExportModal';
 
 export default function extendUserSettingsPage() {
-  extend(SettingsPage.prototype, 'settingsItems', function (items: ItemList<Mithril.Children>) {
+  extend('flarum/forum/components/SettingsPage', 'settingsItems', function (items: ItemList<Mithril.Children>) {
     const user = this.user;
 
     if (!user) {
@@ -28,36 +27,35 @@ export default function extendUserSettingsPage() {
     );
   });
 
-  /** @ts-ignore */
-  SettingsPage.prototype.dataItems = function (): ItemList<Mithril.Children> {
-    const items = new ItemList<Mithril.Children>();
+    override('flarum/forum/components/SettingsPage', 'dataItems', function (): ItemList<Mithril.Children> {
+        const items = new ItemList<Mithril.Children>();
 
-    items.add(
-      'gdprErasure',
-      <div className="gdprErasure-container">
-        <Button className="Button Button-gdprErasure" icon="fas fa-user-minus" onclick={() => app.modal.show(RequestErasureModal)}>
-          {app.translator.trans('blomstra-gdpr.forum.settings.request_erasure_button')}
-        </Button>
-        <p className="helpText">{app.translator.trans('blomstra-gdpr.forum.settings.request_erasure_help')}</p>
-      </div>,
-      50
-    );
+        items.add(
+          'gdprErasure',
+          <div className="gdprErasure-container">
+            <Button className="Button Button-gdprErasure" icon="fas fa-user-minus" onclick={() => app.modal.show(RequestErasureModal)}>
+              {app.translator.trans('blomstra-gdpr.forum.settings.request_erasure_button')}
+            </Button>
+            <p className="helpText">{app.translator.trans('blomstra-gdpr.forum.settings.request_erasure_help')}</p>
+          </div>,
+          50
+        );
 
-    items.add(
-      'gdprExport',
-      <div className="gdprExport-container">
-        <Button
-          className="Button Button-gdprExport"
-          icon="fas fa-file-export"
-          onclick={() => app.modal.show(RequestDataExportModal, { user: this.user })}
-        >
-          {app.translator.trans('blomstra-gdpr.forum.settings.export_data_button')}
-        </Button>
-        <p className="helpText">{app.translator.trans('blomstra-gdpr.forum.settings.export_data_help')}</p>
-      </div>,
-      40
-    );
+        items.add(
+          'gdprExport',
+          <div className="gdprExport-container">
+            <Button
+              className="Button Button-gdprExport"
+              icon="fas fa-file-export"
+              onclick={() => app.modal.show(RequestDataExportModal, { user: this.user })}
+            >
+              {app.translator.trans('blomstra-gdpr.forum.settings.export_data_button')}
+            </Button>
+            <p className="helpText">{app.translator.trans('blomstra-gdpr.forum.settings.export_data_help')}</p>
+          </div>,
+          40
+        );
 
-    return items;
-  };
+        return items;
+});
 }
