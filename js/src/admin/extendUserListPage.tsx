bd137dd7791c70a874700fdb4dfa50585359398b
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2025-02-11 07:40:19 +0100

    chore: increase type safety

--- js/src/admin/extendUserListPage.tsx
+++ js/src/admin/extendUserListPage.tsx
@@ -5,8 +5,12 @@ import Button from 'flarum/common/components/Button';
 import username from 'flarum/common/helpers/username';
 import RequestDataExportModal from '../common/components/RequestDataExportModal';
 
+import type User from 'flarum/common/models/User';
+import type ItemList from 'flarum/common/utils/ItemList';
+import type Mithril from 'mithril';
+
 export default function extendUserListPage() {
-  extend(UserListPage.prototype, 'userActionItems', function (items, user) {
+  extend(UserListPage.prototype, 'userActionItems', function (items: ItemList<Mithril.Children>, user: User) {
     if (!user.canModerateExports()) return;
     items.add(
       'export-data',
