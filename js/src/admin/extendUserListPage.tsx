cefe7728ee9bbc046d4cc0e1cbce547956e55c92
Davide Iadeluca <146922689+DavideIadeluca@users.noreply.github.com>
2025-02-14 10:51:49 +0100

    chore: minor admin frontend cleanup (#60)
    * chore: remove unused classname
    
    This change left aligns the button in the dropdown
    
    * fix: add title for adminnav button
    
    * chore: increase type safety
    
    * chore: rework some admin translations
    
    * Ditch `username()` for use in translations
    * Rename translation keys to better match structure of core
    
    * fix: extract text

--- js/src/admin/extendUserListPage.tsx
+++ js/src/admin/extendUserListPage.tsx
@@ -2,16 +2,23 @@ import app from 'flarum/admin/app';
 import { extend } from 'flarum/common/extend';
 import UserListPage from 'flarum/admin/components/UserListPage';
 import Button from 'flarum/common/components/Button';
-import username from 'flarum/common/helpers/username';
 import RequestDataExportModal from '../common/components/RequestDataExportModal';
 
+import type User from 'flarum/common/models/User';
+import type ItemList from 'flarum/common/utils/ItemList';
+import type Mithril from 'mithril';
+
 export default function extendUserListPage() {
-  extend(UserListPage.prototype, 'userActionItems', function (items, user) {
+  extend(UserListPage.prototype, 'userActionItems', function (items: ItemList<Mithril.Children>, user: User) {
     if (!user.canModerateExports()) return;
     items.add(
       'export-data',
-      <Button className="Button" icon="fas fa-file-export" onclick={() => app.modal.show(RequestDataExportModal, { user: user })}>
-        {app.translator.trans('flarum-gdpr.admin.userlist.columns.gdpr_actions.export', { username: username(user) })}
+      <Button
+        icon="fas fa-file-export"
+        title={app.translator.trans('flarum-gdpr.admin.userlist.columns.user_actions.data_export.tooltip', { username: user.displayName() }, true)}
+        onclick={() => app.modal.show(RequestDataExportModal, { user: user })}
+      >
+        {app.translator.trans('flarum-gdpr.admin.userlist.columns.user_actions.data_export.button')}
       </Button>
     );
   });
